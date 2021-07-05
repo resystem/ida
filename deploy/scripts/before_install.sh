8a07028cf92ae5ffa3f83e329e1221697666bf98
Isaque Costa <isaquebc@gmail.com>
2021-07-05 15:46:37 -0300

    Adding deploy files

--- deploy/scripts/before_install.sh
+++ deploy/scripts/before_install.sh
@@ -2,9 +2,5 @@
 #!/bin/bash
 
 #download node and npm
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
-. ~/.nvm/nvm.sh
-nvm install 14
-
 rm -rf /home/ubuntu/ida-accounts/dist/*
 rm /etc/nginx/sites-enabled/default
